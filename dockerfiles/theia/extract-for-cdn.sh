6fca4b326f2f53f8a55db6bcd5f29cba0055d2f3
jhamohneesh <78133085+jhamohneesh@users.noreply.github.com>
2021-04-19 23:39:24 +0530

    Update extract-for-cdn.sh

--- dockerfiles/theia/extract-for-cdn.sh
+++ dockerfiles/theia/extract-for-cdn.sh
@@ -10,30 +10,39 @@
 help() {
   echo "$0 should be used with 2 parameters: <docker image reference> <local folder where files should be extracted>"
 }
-
+echo "123456789"
 image="$1"
 destination="$2"
 if [ -z "${image}" -o -z "${destination}" ]; then
   help
   exit 1
 fi
-
+echo "qwertyuiop"
 container=$(docker create "$image")
 if [ -d "$destination" ]; then
   rm -Rf "$destination"
 fi
+echo "asdfghjkl"
 mkdir -p "$destination"
+echo "zxcvbnm,'
 docker cp "$container:/home/theia/lib/cdn.json" "$destination" 2>/dev/null || true
+echo "+++++++++++++++"
 if [ -f "$destination/cdn.json" ]; then
   for file in $(jq --raw-output '.[] | select((has("cdn")) and (has("external")|not)) | .chunk,.resource' "$destination/cdn.json" | grep -v 'null')
   do
+    echo "______________++++++++++++++++_____________________"
     dir=$(dirname "$file")
+    echo "---------------------_________________+++++++++++++++++____________________------------------"
     mkdir -p "$destination/$dir"
+    echo "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
     docker cp "$container:/home/theia/lib/$file" "$destination/$file"
+    echo "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
     if [[ "$file" == *.*.js ]]
     then
       docker cp "$container:/home/theia/lib/$file.map" "$destination/$file.map" || true
+      echo "ccccccccccccccccccccccccccccccccccccccccccccc"
     fi
   done
 fi
+echo "fffffffffffffffffffffffffff"
 docker rm "$container" >/dev/null
