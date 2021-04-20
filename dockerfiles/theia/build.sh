084f11bc4bddef2ebbee086bba5da80924a0639e
jhamohneesh <78133085+jhamohneesh@users.noreply.github.com>
2021-04-20 23:05:25 +0530

    Update build.sh

--- dockerfiles/theia/build.sh
+++ dockerfiles/theia/build.sh
@@ -11,24 +11,29 @@ base_dir=$(cd "$(dirname "$0")"; pwd)
 . "${base_dir}"/../build.include
 
 check_github_limits
-
+echo "qwertyuiopoiuytrewertyuiuytrewqwertyuiuytrewertyuikijuytrewertyuytrertyu"
 DIR=$(cd "$(dirname "$0")"; pwd)
-
+echo "1234567890"
 # create che-theia archive
 echo "Compresing 'che-theia' --> ${DIR}/asset-che-theia.tar.gz"
+echo "++++++++++++++++++++++++++++++++++++++++"
 cd "${DIR}"/../.. && git ls-files -c -o --exclude-standard | tar cf ${DIR}/asset-che-theia.tar  -T -
+echo "aaaaaaaaaaaaa+++++++++++++++++++"
 echo $(git rev-parse --short HEAD) > .git-che-theia-sha1
+echo "bbbbbbbbbbbbbbbbbbbbbb++++++++++++++++++"
 tar rf ${DIR}/asset-che-theia.tar .git-che-theia-sha1
+echo "cccccccccccccccccccccccc+++++++++++++"
 gzip -f ${DIR}/asset-che-theia.tar
-
+echo "cccccccccccccccccccccccc+++++++++++++_______________________"
 # Download plugins
 THEIA_YEOMAN_PLUGIN="${DIR}/asset-untagged-theia_yeoman_plugin.theia"
+echo "cccccccccccccccccccccccc+++++++++++++-----------------------------------"
 if [ ! -f "${THEIA_YEOMAN_PLUGIN}" ]; then
     curl -L -o ${THEIA_YEOMAN_PLUGIN} https://github.com/eclipse/theia-yeoman-plugin/releases/download/untagged-8a7963262e021dab8ae0/theia_yeoman_plugin.theia
 fi
 
 init --name:theia "$@"
-
+echo "cccccccccccccccccccccccc+++++++++++++********************************"
 if [ "${CDN_PREFIX:-}" != "" ]; then
   BUILD_ARGS+="--build-arg CDN_PREFIX=${CDN_PREFIX} "
 fi
@@ -46,7 +51,7 @@ if [[ -z "$DOCKER_BUILD_TARGET" ]]; then
   LABEL_CONTENT=$(cat "${base_dir}"/theia_artifacts/cdn.json || true 2>/dev/null)
   if [ -n "${LABEL_CONTENT}" ]; then
     echo "Adding the CDN label..."
-    docker buildx build --platform linux/arm64 --label che-plugin.cdn.artifacts="$(echo ${LABEL_CONTENT} | sed 's/ //g')" -t "${IMAGE_NAME}-with-label" -<<EOF
+    docker build --label che-plugin.cdn.artifacts="$(echo ${LABEL_CONTENT} | sed 's/ //g')" -t "${IMAGE_NAME}-with-label" -<<EOF
 FROM ${IMAGE_NAME}
 EOF
     docker tag "${IMAGE_NAME}-with-label" "${IMAGE_NAME}"
