08a980a05a93f09f69cc7e6119328814f9408fbf
jhamohneesh <78133085+jhamohneesh@users.noreply.github.com>
2021-04-20 16:27:00 +0530

    Update extract-for-cdn.sh

--- dockerfiles/theia/extract-for-cdn.sh
+++ dockerfiles/theia/extract-for-cdn.sh
@@ -37,7 +37,6 @@ if [ -f "$destination/cdn.json" ]; then
     echo "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
     docker cp "$container:/home/theia/lib/$file" "$destination/$file"
     echo "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
-    file $file
     if [ "$file" == *.*.js ]; then
       echo "$file"
       docker cp "$container:/home/theia/lib/$file.map" "$destination/$file.map" || true
