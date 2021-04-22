e013918bc8e36f422e2defbe334863bd966b0c2f
jhamohneesh <78133085+jhamohneesh@users.noreply.github.com>
2021-04-22 10:43:06 +0530

    Update build.sh

--- dockerfiles/theia/build.sh
+++ dockerfiles/theia/build.sh
@@ -54,11 +54,13 @@ if [[ -z "$DOCKER_BUILD_TARGET" ]]; then
     echo "Adding the CDN label..."
     if [ "$DOCKER_TARGET_PLATFORM" == "linux/arm64" ]; then       
       docker buildx build --platform linux/arm64 --load  --label che-plugin.cdn.artifacts="$(echo ${LABEL_CONTENT} | sed 's/ //g')" -t "${IMAGE_NAME}-with-label" -<<EOF
-    else 
+      FROM ${IMAGE_NAME}
+      EOF
+   else 
       docker build --label che-plugin.cdn.artifacts="$(echo ${LABEL_CONTENT} | sed 's/ //g')" -t "${IMAGE_NAME}-with-label" -<<EOF
+      FROM ${IMAGE_NAME}
+      EOF
     fi
-FROM ${IMAGE_NAME}
-EOF
     docker tag "${IMAGE_NAME}-with-label" "${IMAGE_NAME}"
     "${base_dir}"/push-cdn-files-to-akamai.sh
   fi
