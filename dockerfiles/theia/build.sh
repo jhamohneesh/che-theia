32c8a3d440ddeee008d14d3a6cba8b27a864acce
jhamohneesh <78133085+jhamohneesh@users.noreply.github.com>
2021-05-03 09:20:54 +0530

    Update build.sh

--- dockerfiles/theia/build.sh
+++ dockerfiles/theia/build.sh
@@ -52,14 +52,14 @@ if [[ -z "$DOCKER_BUILD_TARGET" ]]; then
   LABEL_CONTENT=$(cat "${base_dir}"/theia_artifacts/cdn.json || true 2>/dev/null)
   if [ -n "${LABEL_CONTENT}" ]; then
     echo "Adding the CDN label..."
-    #if [ "$DOCKER_TARGET_PLATFORM" == "linux/arm64" ]; then       
-    docker buildx build --platform linux/arm64 --push  --label che-plugin.cdn.artifacts="$(echo ${LABEL_CONTENT} | sed 's/ //g')" -t "${IMAGE_NAME}-with-label" -<<EOF
+    if [ "$DOCKER_TARGET_PLATFORM" == "linux/arm64" ]; then       
+      docker buildx build --platform linux/arm64 --load  --label che-plugin.cdn.artifacts="$(echo ${LABEL_CONTENT} | sed 's/ //g')" -t "${IMAGE_NAME}-with-label" -<<EOF
 FROM ${IMAGE_NAME}
 EOF
- #  else 
- #     docker build --label che-plugin.cdn.artifacts="$(echo ${LABEL_CONTENT} | sed 's/ //g')" -t "${IMAGE_NAME}-with-label" -<<EOF
-#FROM ${IMAGE_NAME}
-#EOF
+   else 
+      docker build --label che-plugin.cdn.artifacts="$(echo ${LABEL_CONTENT} | sed 's/ //g')" -t "${IMAGE_NAME}-with-label" -<<EOF
+FROM ${IMAGE_NAME}
+EOF
     fi
     docker tag "${IMAGE_NAME}-with-label" "${IMAGE_NAME}"
     "${base_dir}"/push-cdn-files-to-akamai.sh
